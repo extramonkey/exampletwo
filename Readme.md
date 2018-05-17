eff9ce75e5365c8783f4e2fa303f7bbbec5a50f4
extramonkey <extraflipflops@gmail.com>
2018-05-17 09:11:50 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Readme
+
+This is a quick experiment to play about with composer, github, and packagist.
+
+It does absolutely nothing useful at all.
---
+++ src/Base.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Extramonkey\Exampleone;
+
+class Base {
+    
+    public static function hello($name = '')
+    {
+        
+        return 'Hello ' . $name . '!';
+        
+    }
+    
+}
